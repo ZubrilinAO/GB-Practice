1a198e0db342087019ad7759b42153f5ef8e5bff
ZubrilinAO <rusher1711@gmail.com>
2023-01-18 16:00:36 +0500

    Задача 29

---
+++ C#/HomeWork_Seminar_4/task_29/Program.cs
@@ -0,0 +1,16 @@
+﻿/*
+Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран. (числа берете любые)
+*/
+int[] array = GetArray(8);
+Console.WriteLine("[{0}]", string.Join(", ", array));
+
+//--------------Методы--------------
+int[] GetArray(int size)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(1, 101);
+    }
+    return array;
+}
