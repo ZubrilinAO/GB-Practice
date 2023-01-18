2dce7ddcbde592c459163c8f77708f02c18a26a6
ZubrilinAO <rusher1711@gmail.com>
2023-01-18 16:53:29 +0500

    Задача 36

---
+++ C#/HomeWork_Seminar_5/task_36/Program.cs
@@ -0,0 +1,33 @@
+﻿/*
+Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных индексах.
+[3, 7, 23, 12] -> 19
+[-4, -6, 89, 6] -> 0
+*/
+
+int[] array = GetArray(4, -100, 100);
+Console.WriteLine($"[{string.Join(", ", array)}]");
+Console.WriteLine($"Cумма элементов, стоящих на нечётных индексах равна {GetSumOddIndexes(array)}");
+
+//--------------Методы--------------
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return array;
+}
+
+int GetSumOddIndexes(int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i % 2 != 0)
+        {
+            sum += array[i];
+        }
+    }
+    return sum;
+}
