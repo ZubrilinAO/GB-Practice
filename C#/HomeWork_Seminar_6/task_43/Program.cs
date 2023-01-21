181c2303190331d98ad0e295d13bcd92fcf59384
ZubrilinAO <rusher1711@gmail.com>
2023-01-21 22:57:01 +0500

    Задача 43

--- C#/HomeWork_Seminar_6/task_43/Program.cs
+++ C#/HomeWork_Seminar_6/task_43/Program.cs
@@ -1,20 +1,38 @@
 ﻿/*
-Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел строго больше 0 ввёл пользователь.
-0, 7, 8, -2, -2 -> 2
-1, -7, 567, 89, 223-> 4
+Задача 43: Написать программу, которая на вход принимает массив из любого количества элементов (не менее 6)в промежутке от 0 до 100, а на выходе выводит этот же массив, но отсортированный по возрастанию(от меньшего числа к большему).
 */
 
-Console.Write("Введите числа через пробел: ");
-int[] numS = Array.ConvertAll(Console.ReadLine()!.Split(' ', StringSplitOptions.RemoveEmptyEntries), Convert.ToInt32); ;
-Console.WriteLine($"Количество чисел больше 0: {countNumS(numS)}");
+int size = new Random().Next(6, 101);
+int[] array = GetArray(size, 0, 100);
+Console.WriteLine($"[{string.Join(", ", array)}]");
+Console.WriteLine("Отсортированный массив:");
+Console.WriteLine($"[{string.Join(", ", sortArray(array))}]");
 
 //--------------Методы--------------
-int countNumS(int[] nummbers)
+int[] GetArray(int size, int minValue, int maxValue)
 {
-    int count = 0;
-    for (int i = 0; i < nummbers.Length; i++)
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
     {
-        if (nummbers[i] > 0) count++;
+        array[i] = new Random().Next(minValue, maxValue + 1);
     }
-    return count;
+    return array;
+}
+
+int[] sortArray(int[] array)
+{
+    int tmp = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        for (int j = 0; j < array.Length - 1 - i; j++)
+        {
+            if (array[j] > array[j + 1])
+            {
+                tmp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = tmp;
+            }
+        }
+    }
+    return array;
 }
