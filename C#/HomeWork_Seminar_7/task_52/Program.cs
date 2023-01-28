21d66cc8dd7438d1039a77489b321be1c39d1054
ZubrilinAO <rusher1711@gmail.com>
2023-01-28 23:15:10 +0500

    Задача 52

---
+++ C#/HomeWork_Seminar_7/task_52/Program.cs
@@ -0,0 +1,60 @@
+﻿/*
+Задача 52. Задайте двумерный массив из целых чисел.     .
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+*/
+
+Console.Write("Введите количество строк: ");
+int rows = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите количество столбцов: ");
+int columns = int.Parse(Console.ReadLine()!);
+
+int[,] array = GetArray(rows, columns, 0, 10);
+PrintArray(array);
+Console.WriteLine($"Среднее арифметическое каждого столбца: {string.Join("; ", GetAverageForColumn(array))}.");
+
+// -------------------Методы---------------------------------
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+double[] GetAverageForColumn(int[,] array)
+{
+    double[] avg = new double[array.GetLength(1)];
+
+    for (int i = 0; i < array.GetLength(1); i++)
+    {
+        for (int j = 0; j < array.GetLength(0); j++)
+        {
+            avg[i] += array[j, i];
+        }
+        avg[i] = Math.Round(avg[i] / array.GetLength(0), 2);
+    }
+
+    return avg;
+}
