749a89c956b63de97f2d311dd372353e311d8ea0
ZubrilinAO <rusher1711@gmail.com>
2023-01-28 22:15:24 +0500

    Задача 50

---
+++ C#/HomeWork_Seminar_7/task_50/Program.cs
@@ -0,0 +1,66 @@
+﻿/*
+Задача 50. Напишите программу, которая на вход принимает число, и проверяет есть ли такое число в двумерном массиве, а также возвращает сообщение о том, что оно найдено или же указание, что такого элемента нет.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+17 -> такого числа в массиве нет
+*/
+
+Console.Write("Введите количество строк: ");
+int rows = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите количество столбцов: ");
+int columns = int.Parse(Console.ReadLine()!);
+
+int[,] array = GetArray(rows, columns, 0, 100);
+PrintArray(array);
+
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine()!);
+CheckArray(array, num);
+
+// -------------------Методы---------------------------------
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void CheckArray(int[,] array, int num)
+{
+    bool flagCheck = false;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (array[i, j] == num)
+            {
+                flagCheck = true;
+                break;
+            }
+        }
+    }
+
+    if (flagCheck) Console.Write($"Число {num} найдено в массиве.");
+    else Console.Write($"{num} - такого числа в массиве нет.");
+}
