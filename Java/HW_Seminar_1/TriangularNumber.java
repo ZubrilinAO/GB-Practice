1a104b19b49a701e4c75a13a2719b4ad5de8894d
aozubrilin <aozubrilin@gmail.com>
2023-04-17 21:19:43 +0500

    add task TriangularNumber

---
+++ Java/HW_Seminar_1/TriangularNumber.java
@@ -0,0 +1,18 @@
+package HW_Seminar_1;
+/*
+1. Вычислить n-ое треугольного число (сумма чисел от 1 до n), n! (произведение чисел от 1 до n)
+   Перейти по ссылке, реализовать формулу на Java.
+*/
+import java.util.Scanner;
+
+
+public class TriangularNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.printf("number: ");
+        int number = sc.nextInt();
+        int triangular = number * (number + 1) / 2;
+        System.out.println("Треугольное число для " + number + " = " + triangular);
+        sc.close();
+    }
+}
