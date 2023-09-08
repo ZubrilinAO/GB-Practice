4530389f33ced280e1be5a4be1e6323651335cbc
ZubrilinAO <120241642+ZubrilinAO@users.noreply.github.com>
2023-09-08 14:51:53 +0500

    Create HeapSort.java

---
+++ Algorithms/HW_2/HeapSort.java
@@ -0,0 +1,52 @@
+package Seminar_2;
+
+public class HeapSort {
+    public void heapify(int[] array, int n, int i) {
+        int largest = i;
+        int left = 2 * i + 1;
+        int right = 2 * i + 2;
+
+        if (left < n && array[left] > array[largest]) {
+            largest = left;
+        }
+
+        if (right < n && array[right] > array[largest]) {
+            largest = right;
+        }
+
+        if (largest != i) {
+            swap(array, i, largest);
+            heapify(array, n, largest);
+        }
+    }
+
+    public void sort(int[] array) {
+        int n = array.length;
+
+        for (int i = n / 2 - 1; i >= 0; i--) {
+            heapify(array, n, i);
+        }
+
+        for (int i = n - 1; i >= 0; i--) {
+            swap(array, 0, i);
+            heapify(array, i, 0);
+        }
+    }
+
+    public void swap(int[] array, int i, int j) {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+
+    public static void main(String[] args) {
+        int[] array = { 4, 10, 3, 5, 1 };
+        HeapSort heapSort = new HeapSort();
+        heapSort.sort(array);
+
+        System.out.println("Отсортированный массив:");
+        for (int i : array) {
+            System.out.print(i + " ");
+        }
+    }
+}
